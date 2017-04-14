1b91d2ba61c995d6c1e48d6f3e27f217b7b39353
Andrew Taylor <agrahamt@gmail.com>
2017-04-14 18:59:10 -0500

    add exercise03

--- exercise02/js/main.js
+++ exercise02/js/main.js
@@ -1,11 +1,11 @@
-var buttonEl = document.querySelector('.calc-btn');
+var buttonEl = document.querySelector('.calc-btn')
 var combineNumbers = function(x, y) {
-  return x + y;
+  return x + y
 }
 var getResult = function() {
-  var result = combineNumbers(3, 5);
-  return “the result is “ + result;
+  var result = combineNumbers(3, 5)
+  return 'the result is ' + result
 }
 // Your code goes here
 buttonEl.addEventListener("click", getResult)
-/*research: https://www.w3schools.com/jsref/met_element_addeventlistener.asp */
+/* research: https://www.w3schools.com/jsref/met_element_addeventlistener.asp */
