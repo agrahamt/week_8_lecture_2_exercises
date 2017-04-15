304e681e5d3ead9921bd951d76d2066ad502236a
Andrew Taylor <agrahamt@gmail.com>
2017-04-14 19:31:24 -0500

    add exercise04

---
+++ exercise04/js/main.js
@@ -0,0 +1,12 @@
+var checkboxes = document.querySelectorAll('input')
+
+// Pass in the event object
+var customAlert = function (event) {
+  // Alert the event target's value
+  alert(event.target.value)
+}
+
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', customAlert)
+})
+// https://www.w3schools.com/jsref/event_target.asp
